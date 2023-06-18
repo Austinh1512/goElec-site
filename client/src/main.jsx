911213690936f603b74ff38210ce7941f382cc14
Austin Houchens <austinhouchens1510@gmail.com>
2023-06-18 05:57:52 -0400

    start custom theme

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom"
-import App from './App.jsx'
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import './index.css'
+import App from './App.jsx'
+import theme from './theme.js';
+import { ThemeProvider } from '@mui/material';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>,
 )
---
+++ client/src/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#00A67C"
+        },
+        secondary: {
+            main: "#A6002A"
+        }
+    }
+})
+
+export default theme;
