c5b5f77f877c87cb22b8a5ea4f61fa17595a920f
Austin Houchens <austinhouchens1510@gmail.com>
2023-07-03 04:59:22 -0400

    configure initial cors setup

--- server/server.js
+++ server/server.js
@@ -2,8 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const connectDB = require("./config/connectDB");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
 
+//Connect to DB
 connectDB();
+
+//Middleware
+app.use(cors(corsOptions));
 
 //Routes
 const productsRouter = require("./routes/products");
