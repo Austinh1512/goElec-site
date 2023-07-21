16df12ad7821e5f251a5a9cb9f925d970462c781
Austin Houchens <austinhouchens1510@gmail.com>
2023-07-21 04:41:05 -0400

    complete basic design of products layout

---
+++ client/src/components/Product.jsx
@@ -0,0 +1,50 @@
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+import { useTheme } from "@emotion/react";
+
+export default function Product({ id, product }) {
+  const theme = useTheme();
+
+  return (
+    <Card
+      sx={{
+        maxWidth: 300,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="100%"
+          image={product.images[0].url}
+        />
+        <CardContent
+          sx={{
+            bgcolor: "#333",
+            color: "white",
+            display: "flex",
+            flexDirection: "column",
+            minHeight: "180px",
+          }}
+        >
+          <Typography gutterBottom variant="h4">
+            {product.name}
+          </Typography>
+          <Typography variant="subtitle1">
+            Price:
+            <span style={{ color: theme.palette.primary.main }}>
+              {" $" + product.price}
+            </span>
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
--- client/src/pages/Products.jsx
+++ client/src/pages/Products.jsx
@@ -1,13 +1,59 @@
-import { Helmet } from "react-helmet"
+import { useEffect, useState } from "react";
+import { Helmet } from "react-helmet";
+import { useSearchParams } from "react-router-dom";
+import { Card, Container, styled } from "@mui/material";
+import axios from "../api/axios";
+import Grid from "@mui/material/Unstable_Grid2";
+import Product from "../components/Product";
 
 export default function Home() {
-    return (
-        <>
-            <Helmet>
-                <title>goElec | Products</title>
-            </Helmet>
+  const [products, setProducts] = useState([]);
+  const [searchParams] = useSearchParams();
 
-            <h1 style={{color: "white"}}>Products</h1>
-        </>
-    )
+  useEffect(() => {
+    (async () => {
+      const q = searchParams.get("q");
+      const res = await axios.get(`/products?q=${q}`);
+      setProducts(res.data);
+    })();
+  });
+
+  const displayProducts = () => {
+    if (products.length) {
+      return products.map((product) => {
+        return (
+          <Grid key={product._id} lg={4}>
+            <Product id={product._id} product={product} />
+          </Grid>
+        );
+      });
+    }
+  };
+
+  return (
+    <div>
+      <Helmet>
+        <title>goElec | Products</title>
+      </Helmet>
+
+      <h1 style={{ color: "white" }}>Products</h1>
+      <Container fixed>
+        <Grid container spacing={10} justifyContent="center">
+          {displayProducts()}
+          {
+            //Add hidden cards to ensure partial rows are properly aligned
+            products.length % 3 !== 0 && (
+              <>
+                {[...new Array(3 - (products.length % 3))].map((_, i) => (
+                  <Grid key={i} lg={4}>
+                    <Card sx={{ width: 300, visibility: "hidden" }}></Card>
+                  </Grid>
+                ))}
+              </>
+            )
+          }
+        </Grid>
+      </Container>
+    </div>
+  );
 }
