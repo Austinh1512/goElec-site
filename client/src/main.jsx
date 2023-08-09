575133d102bc6133fccd11b4fb0073be958d05b5
Austin Houchens <austinhouchens1510@gmail.com>
2023-08-09 00:00:05 -0400

    fix formatting

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from "react-router-dom"
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import './index.css'
-import App from './App.jsx'
-import theme from './theme.js';
-import { ThemeProvider } from '@mui/material';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import "./index.css";
+import App from "./App.jsx";
+import theme from "./theme.js";
+import { ThemeProvider } from "@mui/material";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </ThemeProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
