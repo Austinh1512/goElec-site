462766147efb25513ae77c51bc1323fa8ab96b17
Austin Houchens <austinhouchens1510@gmail.com>
2023-08-11 21:04:26 -0400

    Change localhost port to 5000

--- server/server.js
+++ server/server.js
@@ -4,17 +4,22 @@ const app = express();
 const connectDB = require("./config/connectDB");
 const cors = require("cors");
 const corsOptions = require("./config/corsOptions");
+const passport = require("passport");
 
 //Connect to DB
 connectDB();
 
 //Middleware
 app.use(cors(corsOptions));
+app.use(passport.initialize());
+
+//Passport Strategy
+require("./config/passport-jwt");
 
 //Routes
 const productsRouter = require("./routes/products");
 app.use("/api/products", productsRouter);
 
-app.listen(3000, () => {
+app.listen(5000, () => {
   console.log("Server running...");
 });
