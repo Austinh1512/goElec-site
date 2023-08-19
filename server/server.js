c5e109d20ca9ef177a631cdea0356d3123b9bf23
Austin Houchens <austinhouchens1510@gmail.com>
2023-08-19 14:48:07 -0400

    Add middlewares and routers

--- server/server.js
+++ server/server.js
@@ -5,12 +5,17 @@ const connectDB = require("./config/connectDB");
 const cors = require("cors");
 const corsOptions = require("./config/corsOptions");
 const passport = require("passport");
+const cookieParser = require("cookie-parser");
+const setHeaders = require("./middleware/setHeaders");
 
 //Connect to DB
 connectDB();
 
 //Middleware
+app.use(setHeaders);
 app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
 app.use(passport.initialize());
 
 //Passport Strategies
@@ -21,8 +26,11 @@ require("./config/passport-oauth");
 const productsRouter = require("./routes/products");
 app.use("/api/products", productsRouter);
 
-const OAuthRouter = require("./routes/auth/oauth");
-app.use("/api/auth", OAuthRouter);
+const authRouter = require("./routes/auth/jwt");
+app.use("/api/auth", authRouter);
+
+const googleOAuthRouter = require("./routes/auth/oauth");
+app.use("/api/auth/google", googleOAuthRouter);
 
 app.listen(5000, () => {
   console.log("Server running...");
