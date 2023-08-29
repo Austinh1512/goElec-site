d59b781efe05a566d2dd7e9161acc3ee5882537a
Austin Houchens <austinhouchens1510@gmail.com>
2023-08-29 00:52:35 -0400

    Add login and register controllers

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 const User = require("../models/User");
 
 module.exports.handleRefresh = async (req, res) => {
@@ -21,4 +22,57 @@ module.exports.handleRefresh = async (req, res) => {
     { expiresIn: "30s" }
   );
   res.status(200).json({ userID: user._id, accessToken });
+};
+
+module.exports.handleLogin = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return res.status(404).json({ error: "Incorrect credentials." });
+  }
+
+  const match = await bcrypt.compare(password, user.password);
+
+  if (!match) {
+    return res.status(401).json({ error: "Incorrect credentials." });
+  }
+
+  const refreshToken = jwt.sign(
+    { userID: user._id },
+    process.env.JWT_REFRESH_TOKEN_SECRET,
+    { expiresIn: "7d" }
+  );
+  user.refreshToken = refreshToken;
+  const accessToken = jwt.sign(
+    { userID: user._id },
+    process.env.JWT_ACCESS_TOKEN_SECRET,
+    { expiresIn: "30s" }
+  );
+  res.cookie("jwt", refreshToken, { maxAge: 1000 * 60 * 60 * 24 * 7 });
+  res.cookie("accessToken", accessToken, { maxAge: 1000 * 30 });
+  res.status(200).json({ userID: user._id });
+};
+
+module.exports.handleRegister = async (req, res) => {
+  const { email, displayName, password } = req.body;
+  const user = new User({ email, displayName });
+  const saltRounds = 10;
+  const hash = await bcrypt.hash(password, saltRounds);
+  user.password = hash;
+  const refreshToken = jwt.sign(
+    { userID: user._id },
+    process.env.JWT_REFRESH_TOKEN_SECRET,
+    { expiresIn: "7d" }
+  );
+  user.refreshToken = refreshToken;
+  const accessToken = jwt.sign(
+    { userID: user._id },
+    process.env.JWT_ACCESS_TOKEN_SECRET,
+    { expiresIn: "30s" }
+  );
+  res.cookie("jwt", refreshToken, { maxAge: 1000 * 60 * 60 * 24 * 7 });
+  res.cookie("accessToken", accessToken, { maxAge: 1000 * 30 });
+  await user.save();
+  res.status(200).json({ userID: user._id });
 };
